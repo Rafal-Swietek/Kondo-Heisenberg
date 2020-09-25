b13ea1f6d5bc3fdfc2237940890e9d52e3c09420
Rafal Swietek <rafaello@ui.plgrid.wcss.wroc.pl>
2020-09-25 22:07:25 +0200

    Final Friday remarks

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,7 +1,7 @@
 #PBS -S /bin/bash
 #PBS -q main
 #PBS -l walltime=95:59:59
-#PBS -l select=1:mem=16GB
+#PBS -l select=1:mem=32GB
 
 module load Armadillo
 module load mkl
@@ -10,4 +10,5 @@ cd $PBS_O_WORKDIR/
 
 g++ -g main.cpp Hamiltonian.cpp -o program.o -larmadillo -std=c++17
 
-valgrind --leak-check=full --show-reachable=yes --track-origins=yes ./program.o >& log.txt
+#valgrind --leak-check=full --show-reachable=yes --track-origins=yes ./program.o >& log.txt
+./program.o >& log.txt
