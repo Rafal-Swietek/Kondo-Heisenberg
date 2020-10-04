0c97ffa821effd83f2e3f1968e403958903c794a
DESKTOP-9DE0024\77swi <77swietek77@gmail.com>
2020-10-04 12:44:15 +0200

    Test

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main() {
 
     int L = 4; //chain length
-	double t = 1; //electron hopping = t_00
-    double U = 1; // electron repulsion
-	double K = 1; // spin exchange integral*.
-	double J_H = 1; // electron-spin interaction
+	double t = 0.5; //electron hopping = t_00
+    double U = 2.1; // electron repulsion
+	double K = 0.0429; // spin exchange integral*.
+    double J_H = U / 4.; // electron-spin interaction
     int N_e = 4; // numer of electrons - parity same as number of sites
 
 	// Main Program---------------------------------------
