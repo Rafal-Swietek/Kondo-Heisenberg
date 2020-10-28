6e90aeb9cbc43a5e113bc328c2561dde2bd8e396
Rafal Swietek <rafaello@ui.plgrid.wcss.wroc.pl>
2020-10-28 20:11:12 +0100

    works on BEM

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,7 +1,7 @@
 #PBS -S /bin/bash
 #PBS -q main
 #PBS -l walltime=95:59:59
-#PBS -l select=1:mem=32GB
+#PBS -l select=1:mem=64GB
 
 module load Armadillo
 module load mkl
@@ -11,4 +11,4 @@ cd $PBS_O_WORKDIR/
 g++ -g main.cpp Hamiltonian.cpp -o program.o -larmadillo -std=c++17
 
 #valgrind --leak-check=full --show-reachable=yes --track-origins=yes ./program.o >& log.txt
-./program.o >& log.txt
+./program.o >& log_GS_L=14_2.txt
